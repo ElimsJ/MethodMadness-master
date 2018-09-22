fb44262106743ccdbf1d39c62eb74f76bc1b949b
GentleManElims <sjiang9453@bths.edu>
2018-09-21 18:39:47 -0700

    Initial commit

---
+++ src/SJiang/SJiangLib.java
@@ -0,0 +1,17 @@
+package SJiang;
+
+public class SJiangLib
+{
+    public static String cutOut(String mainString, String subString)
+    {
+        String testWord = mainString.substring(0, mainString.indexOf(subString));
+        testWord = testWord+ mainString.substring( mainString.indexOf(subString)+ subString.length());
+        System.out.println(testWord);
+        return testWord;
+    }
+
+    public  static String dateStr("mm/dd/yyyy")
+    {
+        
+    }
+}
---
+++ src/SJiang/TestFile.java
@@ -0,0 +1,12 @@
+package SJiang;
+
+import static SJiang.SJiangLib.cutOut;
+
+public class TestFile
+{
+
+    public static void main(String[] args)
+    {
+        cutOut("for the horde","the");
+    }
+}
