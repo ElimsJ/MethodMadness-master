2ce4675f26c19a73e94cb9a11275789a7a37ae80
GentleManElims <sjiang9453@bths.edu>
2018-09-23 18:59:00 -0400

    3Completed
    3completed

--- src/SJiang/SJiangLib.java
+++ src/SJiang/SJiangLib.java
@@ -2,10 +2,53 @@ package SJiang;
 
 public class SJiangLib
 {
-    public static String cutOut(String mainString, String subString) {
-        String testWord = mainString.substring(0, mainString.indexOf(subString));
-        testWord = testWord + mainString.substring(mainString.indexOf(subString) + subString.length());
-        System.out.println(testWord);
+    public static String cutOut(String mainStr, String subStr)
+    {
+        String testWord = mainStr.substring(0, mainStr.indexOf(subStr));//the first word until it sense the substring word
+        testWord = testWord + mainStr.substring(mainStr.indexOf(subStr) + subStr.length());//skipping the substring and adding the word after substring to the word
+        System.out.println(testWord);//display
         return testWord;
+    }
+    public static boolean isFibonnaci(int num)
+    {
+        int input = num;
+        int num1 = 0;
+        int num2 = 1;
+        int num3 = 0;
+        //fibonnaci basics
+        while (num3 < input)//until it reaches the number
+        {
+            num3 = num1 + num2;//num +
+            num1 = num2;
+            num2 = num3;
+        }
+
+        if(num3 == input)
+        {
+            System.out.println(true);
+            return true;
+        }
+        else if (num3 != input)
+        {
+            System.out.println(false);
+        }
+        return false;
+    }
+    public static boolean isPalindrome(String word) {
+        int length = word.length();
+        if (length < 2)
+        {
+            return true;
+        }
+        else {
+            if (word.charAt(0) != word.charAt(length - 1)) {
+                System.out.println(false);
+            }
+            if (word.charAt(0) == word.charAt(length - 1)) {
+                System.out.println(true);
+                return true;
+            }
+        }
+    return false;
     }
 }
--- src/SJiang/TestFile.java
+++ src/SJiang/TestFile.java
@@ -1,6 +1,8 @@
 package SJiang;
 
 import static SJiang.SJiangLib.cutOut;
+import static SJiang.SJiangLib.isFibonnaci;
+import static SJiang.SJiangLib.isPalindrome;
 
 public class TestFile
 {
@@ -8,5 +10,8 @@ public class TestFile
     public static void main(String[] args)
     {
         cutOut("for rest alliance ","alliance");
+        isFibonnaci(Integer.parseInt("34"));
+        isPalindrome("racecard");
     }
+
 }
